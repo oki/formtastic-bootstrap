14a5f9249883dd4814d736b748481718bd7bb876
Michał Kurek <michal@md6.org>
2022-10-20 10:52:08 +0200

    Changes.

--- lib/formtastic-bootstrap/inputs/base/hints.rb
+++ lib/formtastic-bootstrap/inputs/base/hints.rb
@@ -14,7 +14,8 @@ module FormtasticBootstrap
             end
             template.content_tag(
               :span,
-              Formtastic::Util.html_safe(hint_text),
+              # Formtastic::Util.html_safe(hint_text),
+              hint_text.html_safe,
               :class => hint_class
             )
           end
